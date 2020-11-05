9bb35203b604f4ccadad60e263a0e1a697a22ef8
evereichmann <eve.reichmann@gmail.com>
2020-11-05 10:53:29 -0600

    active storage gem, aws

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,17 +1,18 @@
 class Plant < ApplicationRecord
-    belongs_to :order
-    has_one_attached :avatar
+  belongs_to :order
+  has_one_attached :avatar
 
-    def is_recovered 
-        date = self.arrival_date
-        rec_date = self.recovery_date
+  def recovered?
+  date = self.arrival_date
+  rec_date = self.recovery_date
         if date > rec_date
-           "Not Recovered"
+          "Not Recovered"
         else
-            a = Date.parse("#{date}")
-            b = Date.parse("#{rec_date}")
-            c = b.mjd - a.mjd
-            "#{rec_date.month}/#{rec_date.day}/#{rec_date.year} | #{c} day"     
-        end    
-    end    
+          a = Date.parse("#{date}")
+          b = Date.parse("#{rec_date}")
+          c = b.mjd - a.mjd
+          "#{rec_date.month}/#{rec_date.day}/#{rec_date.year} | #{c} day"
+        end
+  end
+
 end
